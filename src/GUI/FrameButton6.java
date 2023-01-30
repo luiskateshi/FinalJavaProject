1c80f349f7096ecf7298f1bed4e3dbb62d23dcaf
luiskateshi <114147618+luiskateshi@users.noreply.github.com>
2023-01-30 01:21:18 +0100

    Initial commit

---
+++ src/GUI/FrameButton6.java
@@ -0,0 +1,29 @@
+package GUI;
+import Main.*;
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.awt.*;
+import java.util.ArrayList;
+
+public class FrameButton6 {
+    public FrameButton6(ArrayList<Gara> races) {
+
+
+        String[] columnNames = {"Data","Vendodhja", "Emri shoferit", "Emri skuadres", "Pozicioni"};
+        DefaultTableModel model = new DefaultTableModel(columnNames, 0);
+
+        for (Gara row : races) {
+            Object[] data = {row.getData(), row.getVendndodhja(), row.getEmriShoferit(), row.getEmriSkuadres(), row.getPozicioni()};
+            model.addRow(data);
+        }
+
+        JTable table = new JTable(model);
+        JFrame frame = new JFrame("ARenditja e garave sipas datave");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().add(new JScrollPane(table), BorderLayout.CENTER);
+        frame.setPreferredSize(new Dimension(450, 200));
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+}
---
+++ src/Main/MenaxhimKampionati.java
@@ -0,0 +1,6 @@
+package Main;
+
+public interface MenaxhimKampionati {
+    static String file1 = "shoferet.txt";
+    static String file2 = "garat.txt";
+}
